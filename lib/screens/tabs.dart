ce76a63db410f7885e61e4b286d03ce561147047
NazaboyZ <josefleite.00@hotmail.com>
2026-05-31 14:43:24 +0200

    fix: fixt Favorite issue with unfavorasiton

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -16,11 +16,13 @@ class _TabsScreenState extends State<TabsScreen> {
 
   void _toggleMealFavoriteStatus(Meal meal) {
     final isExisting = _favoriteMeals.contains(meal);
-    if (isExisting) {
-      _favoriteMeals.remove(meal);
-    } else {
-      _favoriteMeals.add(meal);
-    }
+    setState(() {
+      if (isExisting) {
+        _favoriteMeals.remove(meal);
+      } else {
+        _favoriteMeals.add(meal);
+      }
+    });
   }
 
   void _selectPage(int index) {
